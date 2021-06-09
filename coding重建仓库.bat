c8486425278683d5bf058558a5068c8ac612b9d6
musicro <1028418@qq.com>
2021-06-10 01:45:29 +0800

    auto push

--- coding重建仓库.bat
+++ coding重建仓库.bat
@@ -8,7 +8,7 @@ git commit -m "create"
 git remote add origin https://e.coding.net/musicrossoft/web/web.git
 git push -f -u origin master
 
-git remote add githubio https://github.com/1028417/1028417.github.io.git
+git remote add githubio https://hub.fastgit.org/1028417/1028417.github.io.git
 git push -f -u githubio master
 
 pause
--- push.bat
+++ push.bat
@@ -4,7 +4,7 @@ git commit -a -m "auto push"
 
 git push -u origin master
 
-git remote add githubio https://github.com/1028417/1028417.github.io.git
+git remote add githubio https://hub.fastgit.org/1028417/1028417.github.io.git
 git push -f -u githubio master
 
 pause
