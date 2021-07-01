ce64d273b4f2e7f94b7c65a331617fb48f43d608
musicro <1028418@qq.com>
2021-07-02 04:16:06 +0800

    x

--- deploy_mdl33.bat
+++ deploy_mdl33.bat
@@ -1,7 +1,7 @@
 cd /d %~dp0
 
-..\XMusic\deploy\curl-win64-mingw\curl  -T  .\web\mdl33\mdl      -u  1028417@qq.com:xMusic#2020  "https://xmusic-generic.pkg.coding.net/xmusic/mdl33/mdl"
-..\XMusic\deploy\curl-win64-mingw\curl  -T  .\web\mdl33\mdlconf  -u  1028417@qq.com:xMusic#2020  "https://xmusic-generic.pkg.coding.net/xmusic/mdl33/mdlconf"
+..\XMusic\bin\curl  -T  .\web\mdl33\mdl      -u  1028417@qq.com:xMusic#2020  "https://xmusic-generic.pkg.coding.net/xmusic/mdl33/mdl"
+..\XMusic\bin\curl  -T  .\web\mdl33\mdlconf  -u  1028417@qq.com:xMusic#2020  "https://xmusic-generic.pkg.coding.net/xmusic/mdl33/mdlconf"
 
 git commit ./web/mdl33/* -m "auto deploy"
 git commit ./index.html -m "auto deploy"
--- deploy_mdl35.bat
+++ deploy_mdl35.bat
@@ -1,7 +1,7 @@
 cd /d %~dp0
 
-..\XMusic\deploy\curl-win64-mingw\curl  -T  .\web\mdl35\mdl      -u  1028417@qq.com:xMusic#2020  "https://musicrossoft-generic.pkg.coding.net/web/mdl35/mdl"
-..\XMusic\deploy\curl-win64-mingw\curl  -T  .\web\mdl35\mdlconf  -u  1028417@qq.com:xMusic#2020  "https://musicrossoft-generic.pkg.coding.net/web/mdl35/mdlconf"
+..\XMusic\bin\curl  -T  .\web\mdl35\mdl      -u  1028417@qq.com:xMusic#2020  "https://musicrossoft-generic.pkg.coding.net/web/mdl35/mdl"
+..\XMusic\bin\curl  -T  .\web\mdl35\mdlconf  -u  1028417@qq.com:xMusic#2020  "https://musicrossoft-generic.pkg.coding.net/web/mdl35/mdlconf"
 
 git commit ./web/mdl35/* -m "auto deploy"
 git commit ./index.html -m "auto deploy"
